0981d7f23698108a4cb6cfdb9e1a47b766b686e8
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-14 09:38:00 +0000

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      characters: ""
+    };
   }
+  
+   maxCharacters = event => {
+    this.setState({
+      characters: event.target.value,
+    });
+}
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" onChange={this.maxCharacters} value={this.state.characters}/>
+        {this.props.maxChars-this.state.characters.length}
       </div>
     );
   }
